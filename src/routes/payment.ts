5b967c7be38bfb031c2d1f850e7db0502a808060
EddyPBR <edvaldojunior1310@gmail.com>
2023-07-04 00:51:00 -0300

    :sparkles: implements Strategy Pattern

--- src/routes/payment.ts
+++ src/routes/payment.ts
@@ -1,21 +1,43 @@
 import { FastifyInstance } from "fastify";
 import { z } from "zod";
 
-import { PurchaseService } from "../services/PurchaseService";
+import {
+  PaymentService,
+  CashService,
+  CreditCardService,
+  PixService,
+  PurchaseService,
+} from "../services/PurchaseService";
 
 export async function paymentRoutes(fastify: FastifyInstance) {
   fastify.post("/purchase", async (request, reply) => {
     const createPurchaseBody = z.object({
-      paymentMethod: z.string(), 
+      paymentMethod: z.string(),
       amount: z.number().gte(0),
     });
 
     const { amount, paymentMethod } = createPurchaseBody.parse(request.body);
-    
-    const purchaseService = new PurchaseService();
 
-    const purchaseResult = purchaseService.purchase(paymentMethod, amount);
+    let paymentService: PaymentService;
+
+    switch (paymentMethod) {
+      case "CASH":
+        paymentService = new CashService();
+        break;
+      case "CREDIT_CARD":
+        paymentService = new CreditCardService();
+        break;
+      case "PIX":
+        paymentService = new PixService();
+        break;
+      default:
+        return reply.status(400).send({ message: "Invalid payment method" });
+    }
+
+    const purchaseService = new PurchaseService(paymentService);
+
+    const purchaseResult = purchaseService.purchase(amount);
 
     return { message: purchaseResult };
-  })
+  });
 }
--- src/services/PurchaseService.ts
+++ src/services/PurchaseService.ts
@@ -2,24 +2,49 @@ import { CashPaymentService } from "./payments/CashPaymentService";
 import { CreditCardPaymentService } from "./payments/CreditCardPaymentService";
 import { PixPaymentService } from "./payments/PixPaymentService";
 
+// STRATEGY
+export interface PaymentService {
+  purchase(amount: number): string;
+}
+
+// CONCRETE STRATEGY
+export class CashService implements PaymentService {
+  purchase(amount: number) {
+    const cashPaymentService = new CashPaymentService();
+    return cashPaymentService.processPayment(amount);
+  }
+}
+
+export class CreditCardService implements PaymentService {
+  purchase(amount: number) {
+    const creditCardPaymentService = new CreditCardPaymentService();
+    return creditCardPaymentService.processPayment(amount);
+  }
+}
+
+export class PixService implements PaymentService {
+  purchase(amount: number) {
+    const pixPaymentService = new PixPaymentService();
+    return pixPaymentService.processPayment(amount);
+  }
+}
+
+// CONTEXT
 export class PurchaseService {
 
-  purchase(paymentMethod: string, amount: number): string {
-    if (paymentMethod === "CASH") {
-      const cashPaymentService = new CashPaymentService();
-      return cashPaymentService.processPayment(amount);
+  private service: PaymentService;
+  
+  constructor(service: PaymentService) {
+    this.service = service;
+  }
 
-    } else if (paymentMethod === "CREDIT_CARD") {
-      const creditCardPaymentService = new CreditCardPaymentService();
-      return creditCardPaymentService.processPayment(amount);
+  setService(service: PaymentService) {
+    this.service = service;
+  }
 
-    } else if (paymentMethod === "PIX") {
-      const pixPaymentService = new PixPaymentService();
-      return pixPaymentService.processPayment(amount);
-
-    } else {
-      throw new Error("Método de pagamento inválido!");
-    }
+  purchase(amount: number) {
+    const purchaseStatus = this.service.purchase(amount);
+    return purchaseStatus;
   }
 
 }
